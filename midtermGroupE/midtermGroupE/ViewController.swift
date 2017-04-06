ce4953a17623764b0a538c35ead32326fb9e4954
MichaelXiao <y19938222@gmail.com>
2017-04-06 17:35:15 +0800

    first commit deployee environment

---
+++ midtermGroupE/midtermGroupE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  midtermGroupE
+//
+//  Created by 孟軒蕭 on 06/04/2017.
+//  Copyright © 2017 MichaelXiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
