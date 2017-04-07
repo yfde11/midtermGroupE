b81d26ed52399194077f01312349b64ee638a517
MichaelXiao <y19938222@gmail.com>
2017-04-07 14:09:21 +0800

    before merge and doing pick image

--- midtermGroupE/midtermGroupE/AddJournalsViewController.swift
+++ midtermGroupE/midtermGroupE/AddJournalsViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Fusuma
 
-class AddJournalsViewController: UIViewController {
+class AddJournalsViewController: UIViewController, FusumaDelegate {
 
     @IBOutlet weak var journalImagePicker: UIImageView!
     @IBOutlet weak var journalTitle: UITextField!
@@ -25,4 +25,42 @@ class AddJournalsViewController: UIViewController {
         JournalManager.shared.saveCoreData(title: journalTitle.text!, content: content.text!, time: Date(), picture: imageData)
 
     }
+    @IBAction func selectImage(_ sender: Any) {
+        let fusuma = FusumaViewController()
+        fusuma.delegate = self
+        fusuma.cropHeightRatio = 0.6
+        self.present(fusuma, animated: true, completion: nil)
+    }
+
+    // Return the image which is selected from camera roll or is taken via the camera.
+    func fusumaImageSelected(image: UIImage) {
+
+        print("Image selected")
+    }
+
+    // Return the image but called after is dismissed.
+    func fusumaDismissedWithImage(image: UIImage) {
+
+        print("Called just after FusumaViewController is dismissed.")
+    }
+
+    func fusumaVideoCompleted(withFileURL fileURL: URL) {
+
+        print("Called just after a video has been selected.")
+    }
+
+    // When camera roll is not authorized, this method is called.
+    func fusumaCameraRollUnauthorized() {
+
+        print("Camera roll unauthorized")
+    }
+    
+    func fusumaClosed() {
+        print("Called when the FusumaViewController disappeared")
+    }
+    
+    func fusumaWillClosed() {
+        print("Called when the close button is pressed")
+    }
+
 }
