86d73f20b8b47f7a4ac1ee7b74129af900fcf5f9
MichaelXiao <y19938222@gmail.com>
2017-04-07 13:19:06 +0800

    establish edit objects connection

--- midtermGroupE/midtermGroupE/EditJournalViewController.swift
+++ midtermGroupE/midtermGroupE/EditJournalViewController.swift
@@ -9,27 +9,15 @@
 import UIKit
 
 class EditJournalViewController: UIViewController {
+    @IBOutlet weak var editImage: UIImageView!
 
+    @IBOutlet weak var editTitle: UITextField!
+    @IBOutlet weak var editContent: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func finishEdit(_ sender: Any) {
+        print("Edit success")
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
