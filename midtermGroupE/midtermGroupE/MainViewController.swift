6b9662cc4773670afe29d054f490c4bf40d0bd31
CelesteTang <optimism2025@gmail.com>
2017-04-07 10:37:35 +0800

    Add entity

--- midtermGroupE/midtermGroupE/MainViewController.swift
+++ midtermGroupE/midtermGroupE/MainViewController.swift
@@ -20,7 +20,6 @@ class MainViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
