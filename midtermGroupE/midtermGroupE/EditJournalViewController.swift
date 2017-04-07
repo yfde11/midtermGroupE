cd025646d8e73badcc8acadc41863750265c3399
MichaelXiao <y19938222@gmail.com>
2017-04-07 16:33:35 +0800

    finish

--- midtermGroupE/midtermGroupE/EditJournalViewController.swift
+++ midtermGroupE/midtermGroupE/EditJournalViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import Fusuma
 
-class EditJournalViewController: UIViewController {
+class EditJournalViewController: UIViewController, FusumaDelegate {
 
     var receivedJournals: [Journal] = []
 
@@ -25,10 +26,76 @@ class EditJournalViewController: UIViewController {
     }
     @IBAction func finishEdit(_ sender: Any) {
         print("Edit success")
+        let imageData = NSData(data: UIImageJPEGRepresentation(editImage.image!, 1.0)!)
 
-        JournalManager.shared.saveCoreData(title: editTitle.text!, content: editContent.text!, time: receivedJournals[0].time, picture: receivedJournals[0].picture)
+        JournalManager.shared.saveCoreData(title: editTitle.text!, content: editContent.text!, time: receivedJournals[0].time, picture: imageData)
         
         let vc = self.storyboard?.instantiateViewController(withIdentifier: "root")
         self.present(vc!, animated: true, completion: nil)
     }
+    @IBAction func selectImage(_ sender: Any) {
+        let fusuma = FusumaViewController()
+        fusuma.delegate = self
+        fusuma.cropHeightRatio = 0.6
+        self.present(fusuma, animated: true, completion: nil)
+    }
+    
+    // Return the image which is selected from camera roll or is taken via the camera.
+    func fusumaImageSelected(image: UIImage) {
+        
+        print("Image selected")
+    }
+    
+    // Return the image but called after is dismissed.
+    func fusumaDismissedWithImage(image: UIImage) {
+        
+        print("Called just after FusumaViewController is dismissed.")
+    }
+    
+    func fusumaVideoCompleted(withFileURL fileURL: URL) {
+        
+        print("Called just after a video has been selected.")
+    }
+    
+    func fusumaCameraRollUnauthorized() {
+        
+        print("Camera roll unauthorized")
+        
+        let alert = UIAlertController(title: "Access Requested", message: "Saving image needs to access your photo album", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Settings", style: .default, handler: { (_) -> Void in
+            
+            if let url = URL(string:UIApplicationOpenSettingsURLString) {
+                UIApplication.shared.openURL(url)
+            }
+            
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (_) -> Void in
+            
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    public func fusumaImageSelected(_ image: UIImage, source: FusumaMode) {
+        switch source {
+        case .camera:
+            print("Image captured from Camera")
+        case .library:
+            print("Image selected from Camera Roll")
+        default:
+            print("Image selected")
+        }
+        
+        editImage.image = image
+    }
+    
+    func fusumaClosed() {
+        print("Called when the FusumaViewController disappeared")
+    }
+    
+    func fusumaWillClosed() {
+        print("Called when the close button is pressed")
+    }
+
 }
