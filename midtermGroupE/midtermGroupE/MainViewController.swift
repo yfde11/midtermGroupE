8fcb778dd6fc1bee48bcc21d53e56c9034f9d344
CelesteTang <optimism2025@gmail.com>
2017-04-07 13:06:09 +0800

    Modify mainviewcontroller

--- midtermGroupE/midtermGroupE/MainViewController.swift
+++ midtermGroupE/midtermGroupE/MainViewController.swift
@@ -7,29 +7,61 @@
 //
 
 import UIKit
+import CoreData
 
 class MainViewController: UIViewController {
-
+    
+    var journals: [Journal] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        JournalManager.shared.saveCoreData(title: "333", content: "333", order: 2, picture: NSData())
+        
+        getCoreData()
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func getCoreData() {
+        
+        guard let app = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let context = app.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: Constants.CoreDataKey.entityName)
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            var coreDataJournals = [Journal]()
+            
+            for result in (results as? [NSManagedObject])! {
+                
+                if let title = result.value(forKey: Constants.CoreDataKey.title) as? String,
+                    let content = result.value(forKey: Constants.CoreDataKey.content) as? String,
+                    let order = result.value(forKey: Constants.CoreDataKey.order) as? Int,
+                    let picture = result.value(forKey: Constants.CoreDataKey.picture) as? NSData {
+                    
+                    let coreDataJournal = Journal(title: title, content: content, order: order, picture: picture)
+                    coreDataJournals.append(coreDataJournal)
+                    print(coreDataJournal.title)
+                }
+                
+            }
+            
+            journals = coreDataJournals
+            print(coreDataJournals.count)
+            
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
+        }
+        
     }
-    */
-
+    
 }
+
